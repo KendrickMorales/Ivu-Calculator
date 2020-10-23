71dc16252eafc98f68455d3b311ee23a828a73d7
BuildTools <unconfigured@null.spigotmc.org>
2020-10-22 20:35:19 -0400

    Added Tax Class

--- Tax.py
+++ Tax.py
@@ -1,11 +1,31 @@
 class Tax:
-    def __init__(self):
-        pass
+    def __init__(self,tax,order):
+        self.tax = tax
+        self.order = order
+
+    def sumAllInOrder(self):
+        
+        subTotal = 0
+        for item in self.order:
+            subTotal+=item
+
+        return subTotal
+
+    def calTax(self):
+        subTotal = self.sumAllInOrder()
+
+        newTax  = subTotal * self.tax
+
+        finalTotal = newTax + subTotal
+
+        return finalTotal
+
+    def printResults(self):
+        print("\nSu total antes del tax: " + str(self.sumAllInOrder()) )
+        print("\nSu total final es: " + str(self.calTax()) )
 
 
 
-def main():
-    pass
 
-def if __name__ == "__main__":
-    pass
+
+
